39eadb0f2af0691229ca4c2e4a7c5ba5f8a56db7
PaulAyling <jimmyyukka@gmail.com>
2021-10-16 07:11:35 +0200

    fixed code

--- my-app/src/data/mockup/singleDocument.js
+++ my-app/src/data/mockup/singleDocument.js
@@ -1,16 +1,12 @@
 // This consolidates all the mockups for easy import
 import {documentViews } from './documentViews'
-import {allComponents } from './allComponents'
-import { getDocComponents} from '../dataFunctions/render'
+import { getComponents} from '../dataFunctions/render'
 
 //From prop
 const docId = 1 // user picks this document
 //Document Data
 const documentView = documentViews[docId]
-const componentIds = documentView.all_component_ids
-const documentComponents = getDocComponents(componentIds,allComponents)
-// console.log('SD allComponents',allComponents)
-// console.log('SD documentComponents',documentComponents)
+const documentComponents = getComponents(docId)
 
 const singleDocument={
     documentView,
