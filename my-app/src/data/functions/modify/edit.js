3060422a30b5d5bb57bbeb0696e0763b8a15e773
PaulAyling <jimmyyukka@gmail.com>
2021-12-13 16:49:18 +0100

    completed add,remove & edit

--- my-app/src/data/functions/modify/edit.js
+++ my-app/src/data/functions/modify/edit.js
@@ -1,61 +1,38 @@
 import {singleDocument} from '../../importSingleDocument'
-import { getUsersViewId, getUsersVersion } from '../utils/utils'
+import { getUsersVersion } from '../utils/utils'
 import { arrayIsValueInArray } from '../utils/array_fn'
 import {currentUser} from '../../dbMockup/currentUser'
 
 const util = require('util')
 const contextMock = singleDocument
-console.log(contextMock)
 
 const edit = (fieldToUpdate,newContent,componentId) =>{
     const validFieldsToUpdate = ['title','imageUrl','notes']
     if(arrayIsValueInArray(validFieldsToUpdate,'title') != true){
         return "Error! Field Name is not valid"
     } else{
-
     // A. Create the Data
-        // get documentCompoents
+        // get documentComponents
             const documentComponents = contextMock.documentComponents
-            // console.log('documentComponents',documentComponents)
         // Get the documentComponent
             const documentComponent = contextMock.documentComponents[componentId]
-            // console.log('documentComponent',documentComponent)  
         // Identify document version
-        const docVersions = documentComponent.usersVersion
-        const versionId = getUsersVersion(docVersions)
-        // console.log('versionId',versionId)
+            const docVersions = documentComponent.usersVersion
+            const versionId = getUsersVersion(docVersions)
         // Get the usersComponent
-        const usersComponent = documentComponent.versions[versionId]
-        // console.log('usersComponent',usersComponent)
+            const usersComponent = JSON.parse(JSON.stringify(documentComponent.versions[versionId]))
     // B. create new data parts
         // update field
-        usersComponent[fieldToUpdate]  = newContent
-        // console.log('usersComponent',usersComponent)
-        //update documentComponent
-        const newDocumentComponentsVersions = {...documentComponent.versions, [versionId]:usersComponent}
-        console.log('documentComponent',documentComponent)
-        console.log('newDocumentComponentsVersions',newDocumentComponentsVersions)
-        //update the 
+            usersComponent[fieldToUpdate]  = newContent
+        //update newDocumentComponents
+            const newDocumentComponentsVersions = {...documentComponent.versions, [versionId]:usersComponent}
+            const newDocumentComponent = {...documentComponent,'usersVersions':newDocumentComponentsVersions }
+            const newDocumentComponents = {...documentComponents,[componentId]:newDocumentComponent}
     // C. Create final output
-        
-
-
-    // 1. Get usersViewId
-        const usersViewId = getUsersViewId()
-    // 2. Get userView
-        const documentViews = singleDocument.documentViews
-        const usersView = documentViews[usersViewId]
-    const usersCompnentVersionId = ''
-   
-
-
-
-
+        const res = {...contextMock, 'documentComponents':newDocumentComponents,}
+        // console.log(' edit outputr....',util.inspect(res,{ depth: null }))
+        return res
     }
-
-
-    // console.log('contextMock',util.inspect(contextMock,{ depth: null }))
-    // console.log('componentId:',componentId,'fieldToUpdate:',fieldToUpdate)
 }
 
 export {edit }
